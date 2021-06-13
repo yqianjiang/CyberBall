42f3de2efb93b5009271115853702e7335a70ae3
yqianjiang <y.jiang.psy@gmail.com>
2021-06-14 01:46:15 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# abort on errors
 set -e
 
-# 构建
+# build
 npm run build
 
-# cd 到构建输出的目录下 
-cd dist
+# navigate into the build output directory
+cd docs
 
-git init
 git add -A
 git commit -m 'deploy'
+git push
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yqianjiang/CyberBall.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
