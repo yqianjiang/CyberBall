7abd287c3f6acd3710e1ced76959d5412f93902a
yqianjiang <y.jiang.psy@gmail.com>
2023-10-09 16:24:45 +0800

    setup auto deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: "docs",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
